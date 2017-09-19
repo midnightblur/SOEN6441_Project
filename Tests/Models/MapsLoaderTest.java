ba0c20d2cd55c985091309c12c0c8c697d0a46ff
eugen <eugencaruntu@gmail.com>
2017-09-19 15:20:42 -0400

    jUnit Tests folder

---
+++ Tests/Models/MapsLoaderTest.java
@@ -0,0 +1,22 @@
+package Models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MapsLoaderTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void readMapFile() throws Exception {
+    }
+
+}
