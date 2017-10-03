434284f6660c72e5ab218e2b0517cd439a19c3bc
yashleenvirk <yashleenvirk@gmail.com>
2017-10-03 18:38:07 -0400

    UI initialization

---
+++ src/Views/MainPage.java
@@ -0,0 +1,18 @@
+package Views;
+
+import Controllers.GameMapHandler;
+import Models.RiskGame;
+import Utils.Config;
+
+import javax.swing.*;
+
+public class MainPage {
+    private JPanel Play;
+    private JButton mapEditorButton;
+    private JButton playGameButton;
+    private JButton quitGameButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
