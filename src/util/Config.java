52e816d3389fd3b6cfae00b1fcea65a5ac60d8c3
Quoc Minh VU <minhvq27@gmail.com>
2017-10-05 00:22:45 -0400

    Improve map validation. Now it is able to 1. Check for continent duplicated declaration 2. Check for invalid continent declaration 3. Check for invalid territory/neighbour declaration

--- src/util/Config.java
+++ src/util/Config.java
@@ -4,8 +4,11 @@ import java.awt.*;
 
 public class Config {
     // Constants for reading GameMap Text File
+    public static final int MAPS_MIN_TERRITORIES = 2;
     public static final int MAPS_MAX_TERRITORIES = 255;
+    public static final int MAPS_MIN_CONTINENTS = 1;
     public static final int MAPS_MAX_CONTINENTS = 32;
+    public static final int MAPS_MIN_NEIGHBORS = 1;
     public static final int MAPS_MAX_NEIGHBORS = 10;
     public static final String MAPS_AUTHOR = "author";
     public static final String MAPS_IMAGE = "image";
@@ -49,11 +52,14 @@ public class Config {
     public static final String MSG_MAPFILE_ERROR_TITLE = "The map file has error";
     public static final String MSG_MAPFILE_VALID = "The file map is valid";
     public static final String MSG_MAPFILE_INVALID_FORMAT = "Invalid format text file: line %s";
-    public static final String MSG_MAPFILE_TOO_MANY_TERRITORIES = "Too many territories. Maximum number is " + MAPS_MAX_TERRITORIES;
-    public static final String MSG_MAPFILE_TOO_MANY_CONTINENTS = "Too many continents. Maximum number is " + MAPS_MAX_CONTINENTS;
-    public static final String MSG_MAPFILE_TOO_MANY_NEIGHBORS = "%s has too many neighbors. Maximum number is " + MAPS_MAX_NEIGHBORS;
+    public static final String MSG_MAPFILE_INVALID_TERRITORIES_COUNT = "Invalid number of territories. Minimum number is " + MAPS_MIN_TERRITORIES + ", maximum number is " + MAPS_MAX_TERRITORIES;
+    public static final String MSG_MAPFILE_INVALID_CONTINENTS_COUNT = "Invalid number of continents. Minimum number is " + MAPS_MIN_CONTINENTS + ", maximum number is " + MAPS_MAX_CONTINENTS;
+    public static final String MSG_MAPFILE_INVALID_NEIGHBORS_COUNT = "%s has too many neighbors. Maximum number is " + MAPS_MAX_NEIGHBORS;
     public static final String MSG_MAPFILE_NO_NEIGHBORS = "%s has no neighbor";
     public static final String MSG_MAPFILE_DISCONNECTED_GRAPH = "The graph is not connected";
     public static final String MSG_MAPFILE_1_WAY_RELATIONSHIP = "%s - %s is not a 2 ways relationship";
     public static final String MSG_MAPFILE_NO_IMAGE = "The map image is missing";
+    public static final String MSG_MAPFILE_CONTINENT_NOT_DEFINED = "The continent is not defined: line %s";
+    public static final String MSG_MAPFILE_CONTINENT_DUPLICATED = "The continent already exist: line %s";
+    public static final String MSG_MAPFILE_TERRITORY_NOT_DEFINED = "A territory is not defined";
 }
