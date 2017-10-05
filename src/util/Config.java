6aca5dd05c9d642bd25fdefddff7fcb0f2f8db14
Quoc Minh VU <minhvq27@gmail.com>
2017-10-05 15:51:30 -0400

    Remove constants related to drawing graph and images

--- src/util/Config.java
+++ src/util/Config.java
@@ -1,7 +1,5 @@
 package util;
 
-import java.awt.*;
-
 public class Config {
     // Constants for reading GameMap Text File
     public static final int MAPS_MAX_TERRITORIES = 255;
@@ -24,23 +22,6 @@ public class Config {
     public static final String MAPS_PATH = "Maps/World.map";
 //    public static final String MAPS_PATH = "Maps/001_I72_Ghtroc 720.map";
 //    public static final String MAPS_PATH = "Maps/99 Mens Morris.map";
-
-    // Constants for drawing graph
-    public static final int GRAPH_NODE_WIDTH = 50;
-    public static final int GRAPH_NODE_HEIGHT = 50;
-    public static final Color GRAPH_NODE_COLOR = Color.YELLOW;
-    public static final Color GRAPH_EDGE_COLOR = Color.BLACK;
-    public static final Color GRAPH_BORDER_COLOR = Color.BLACK;
-    public static final int UI_FRAME_WIDTH = 1440;
-    public static final int UI_FRAME_HEIGHT = 850;
-    public static final double GRAPH_WIDTH_SCALE = 1.5;
-    public static final double GRAPH_HEIGHT_SCALE = 2;
-    public static final Color GRAPH_COLOR_P1 = Color.GREEN;
-    public static final Color GRAPH_COLOR_P2 = Color.BLUE;
-    public static final Color GRAPH_COLOR_P3 = Color.RED;
-    public static final Color GRAPH_COLOR_P4 = Color.ORANGE;
-    public static final Color GRAPH_COLOR_P5 = Color.PINK;
-    public static final Color GRAPH_COLOR_P6 = Color.MAGENTA;
 
     // Constants for dice
     public static final int MAX_PIPS = 6;
