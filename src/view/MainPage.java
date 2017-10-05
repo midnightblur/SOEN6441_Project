1dca1e0dfbe1355b3436be32312b1d095bdac241
yashleenvirk <yashleenvirk@gmail.com>
2017-10-05 18:26:10 -0400

    Adding backup tests

---
+++ src/view/MainPage.java
@@ -0,0 +1,31 @@
+package view;
+
+
+
+import javax.swing.*;
+import java.awt.*;
+
+
+import model.RiskGame;
+import util.Config;
+
+import javax.swing.*;
+
+public class MainPage extends JFrame {
+    private JPanel Play;
+    private JButton mapEditorButton;
+    private JButton playGameButton;
+    private JButton quitGameButton;
+
+
+    public MainPage() {
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+       // this.setContentPane(this.mainArea);
+        this.pack();
+        this.setResizable(false);
+        this.setVisible(true);
+    }
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
