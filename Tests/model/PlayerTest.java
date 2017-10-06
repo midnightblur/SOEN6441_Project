6095fc1289de6ba58c10d04aca88f2a2dc08171d
Sachleen Kaur <sachleen19@gmail.com>
2017-10-05 20:45:47 -0400

    Junit Testing

---
+++ Tests/model/PlayerTest.java
@@ -0,0 +1,15 @@
+package model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Sachleen on 10/5/2017.
+ */
+public class PlayerTest {
+    @Test
+    public void getID() throws Exception {
+    }
+
+}
