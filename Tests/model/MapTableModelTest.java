f1075dedf19df1c26b03cb9946728e32c8d81977
Sachleen Kaur <sachleen19@gmail.com>
2017-10-06 09:55:33 -0400

    Junit Testing

---
+++ Tests/model/MapTableModelTest.java
@@ -0,0 +1,15 @@
+package model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Sachleen on 10/6/2017.
+ */
+public class MapTableModelTest {
+    @Test
+    public void updateMapTableModel() throws Exception {
+    }
+
+}
---
+++ Tests/model/RiskGameTest.java
@@ -0,0 +1,15 @@
+package model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Sachleen on 10/6/2017.
+ */
+public class RiskGameTest {
+    @Test
+    public void getInstance() throws Exception {
+    }
+
+}
