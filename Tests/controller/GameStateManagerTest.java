e428311a36829cd5c6bcd3ec44a1e8a26f39e8d0
Sachleen Kaur <sachleen19@gmail.com>
2017-10-05 20:28:08 -0400

    Junit Testing

---
+++ Tests/controller/GameStateManagerTest.java
@@ -0,0 +1,15 @@
+package controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Sachleen on 10/5/2017.
+ */
+public class GameStateManagerTest {
+    @Test
+    public void getGameState() throws Exception {
+    }
+
+}
