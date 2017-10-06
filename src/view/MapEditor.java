6813a1f59abfa4cf5d9223d497b4a6c07c348525
eugen <eugencaruntu@gmail.com>
2017-10-06 10:46:23 -0400

    better MapEditor UI

--- src/view/MapEditor.java
+++ src/view/MapEditor.java
@@ -4,6 +4,8 @@ import model.MapTableModel;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumnModel;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.util.Observable;
@@ -15,7 +17,7 @@ public class MapEditor extends JFrame implements Observer {
     private JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, new JScrollPane(), new JPanel());
     private JScrollPane scrollPane = new JScrollPane();
     private JPanel controlPane = new JPanel();
-    private JLabel pathLabel = new JLabel("Map location: ");
+    private JLabel pathLabel = new JLabel("Chose map: ");
     private JTextField path = new JTextField("World.map");
     private JButton loadMap = new JButton("Load Map");
     private JTable myTable = new JTable();
@@ -27,14 +29,12 @@ public class MapEditor extends JFrame implements Observer {
         controlPane.add(path);
         controlPane.add(loadMap);
         scrollPane.add(myTable);
+        splitPane.setResizeWeight(.85d);
+        splitPane.setDividerLocation(1200);
         splitPane.setRightComponent(controlPane);
         splitPane.setLeftComponent(new JScrollPane(myTable));
         splitPane.setSize(1600, 900);
-        myTable.setFont(new Font("Verdana", Font.PLAIN, 20));
-        myTable.setAutoCreateRowSorter(true);
-        myTable.setRowHeight(24);
-        myTable.setBackground(Color.orange);
-        myTable.setForeground(Color.blue);
+        myTable.setAutoCreateRowSorter(false);
         
         this.display();
     }
@@ -45,17 +45,46 @@ public class MapEditor extends JFrame implements Observer {
         myFrame.add(splitPane);
         myFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         myFrame.pack();
+        myFrame.setSize(1600, 1200);
         myFrame.setLocationRelativeTo(null);
         myFrame.setVisible(true);
     }
     
-    public void setModel(DefaultTableModel tableModel) {
-        myTable.setModel(tableModel);
-    }
-    
+    /* Getters and Setters */
     public String getPath() {
         return path.getText();
     }
+    
+    /**
+     * Sets the table model (data source)
+     *
+     * @param tableModel
+     */
+    public void setModel(DefaultTableModel tableModel) {
+        myTable.setModel(tableModel);
+        resizeColumns(myTable);
+    }
+    
+    /**
+     * Resize the table columns to fit
+     *
+     * @param table
+     */
+    private void resizeColumns(JTable table) {
+        final TableColumnModel columnModel = table.getColumnModel();
+        for (int c = 0; c < table.getColumnCount(); c++) {
+            int width = 10; // minimum width
+            for (int r = 0; r < table.getRowCount(); r++) {
+                TableCellRenderer renderer = table.getCellRenderer(r, c);
+                Component comp = table.prepareRenderer(renderer, r, c);
+                width = Math.max(comp.getPreferredSize().width + 1, width);
+            }
+            if (width > 400)    // cap the width
+                width = 400;
+            columnModel.getColumn(c).setPreferredWidth(width);
+        }
+    }
+    
     
     /**
      * When the button is clicked execute a method in the Controller named loadGameMap
@@ -65,7 +94,6 @@ public class MapEditor extends JFrame implements Observer {
     public void addActionListener(ActionListener MapEditorController) {
         loadMap.addActionListener(MapEditorController);
     }
-    
     
     /**
      * Open a popup that contains the error message passed
@@ -80,4 +108,5 @@ public class MapEditor extends JFrame implements Observer {
     public void update(Observable o, Object arg) {
         myTable.setModel(((MapTableModel) o).getModel());
     }
+    
 }
