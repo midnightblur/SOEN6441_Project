f99d1b8da2ce35fb1c375ee14a82fb68fb504125
yashleenvirk <yashleenvirk@gmail.com>
2017-10-11 15:23:52 -0400

    JUnit test update

--- Tests/model/ContinentTest.java
+++ Tests/model/ContinentTest.java
@@ -1,14 +1,25 @@
 package model;
 
+import model.game_entities.Continent;
 import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * This test case is used to test the continent class.
  */
 public class ContinentTest {
+
+    Continent continent = new Continent("Asia", 5);
+
     @Test
     public void getName() throws Exception {
 
+            int x = 7;
+            String continentCheck = "Asia";
+            assertEquals(continent,continentCheck);
+            assertTrue(x > 0);
 
     }
 
