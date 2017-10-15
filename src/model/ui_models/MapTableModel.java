eb873e89f25e3ffc182eac07e54d63b5b9def7b2
Juyoun Kim <juyoun.brian.kim@gmail.com>
2017-10-15 05:48:19 -0400

    fixed map table compatibility

--- src/model/ui_models/MapTableModel.java
+++ src/model/ui_models/MapTableModel.java
@@ -40,7 +40,7 @@ public class MapTableModel extends Observable {
         columns.add("Continent");
         columns.add("Territory");
         columns.add("Neighbors");
-        if (RiskGame.getInstance().getGameState().getValue() > 3) {
+        if (RiskGame.getInstance().getGameState().getValue() >= 3) {
             columns.add("Owner");
             columns.add("Armies");
         }
@@ -50,7 +50,7 @@ public class MapTableModel extends Observable {
         /* add continents */
         for (Continent continent : gameMap.getContinents().values()) {
             rows[i][0] = continent.getName();
-            if (RiskGame.getInstance().getGameState().getValue() > 3) {
+            if (RiskGame.getInstance().getGameState().getValue() >= 3) {
                 rows[i][3] = continent.getContinentOwner();
                 rows[i][4] = Integer.toString(continent.getContinentArmies());
             }
@@ -63,7 +63,7 @@ public class MapTableModel extends Observable {
             rows[i][2] = territory.getNeighbors().toString().replace("[", "").replace("]", "");
             
             /* add countries and their information */
-            if (RiskGame.getInstance().getGameState().getValue() > 3) {
+            if (RiskGame.getInstance().getGameState().getValue() >= 3) {
                 rows[i][3] = "Player " + Integer.toString(territory.getOwner().getPlayerID());
                 rows[i][4] = Integer.toString(territory.getArmies());
             }
