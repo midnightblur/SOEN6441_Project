8918cc88bdb640f754940e36ff318a42677d9665
Quoc Minh VU <minhvq27@gmail.com>
2017-11-06 21:42:46 -0500

    Refactor Javadoc of update() function of Observer interface

--- src/map_editor/view/EditMapPanel.java
+++ src/map_editor/view/EditMapPanel.java
@@ -135,7 +135,13 @@ public class EditMapPanel extends JPanel implements Observer {
     }
     
     /**
-     * @see java.util.Observer#update(java.util.Observable, java.lang.Object)
+     * This method is called whenever the observed object is changed. An
+     * application calls an <tt>Observable</tt> object's
+     * <code>notifyObservers</code> method to have all the object's
+     * observers notified of the change.
+     *
+     * @param o   the observable object.
+     * @param arg an argument passed to the <code>notifyObservers</code>
      */
     @Override
     public void update(Observable o, Object arg) {
