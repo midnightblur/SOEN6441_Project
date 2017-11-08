949cc6a732b4487c9f4480d875f1b35c9a68f8ae
Juyoun Kim <juyoun.brian.kim@gmail.com>
2017-11-08 12:38:16 -0500

    finish all tests for gameplaymodeltest

--- Tests/game_play/model/GamePlayModelTest.java
+++ Tests/game_play/model/GamePlayModelTest.java
@@ -21,6 +21,7 @@ import java.util.Map;
 import java.util.Vector;
 
 import static org.junit.Assert.assertEquals;
+import static shared_resources.utilities.Config.INITIAL_ARMY_RATIO;
 
 /**
  * This test class is used to test the startup initialization of the GamePlayModel class.
@@ -37,7 +38,7 @@ public class GamePlayModelTest {
 //    - get next player
 //     */
 //
-    GamePlayModel fixedGamePlayModel = FixedGamePlayModel.getFixedGamePlayModel();
+    private GamePlayModel fixedGamePlayModel = FixedGamePlayModel.getFixedGamePlayModel();
     
     /**
      * Test case for initPlayer() in GamePlayModel class.
@@ -108,7 +109,20 @@ public class GamePlayModelTest {
      */
     @Test
     public void giveInitialArmiesTestCase() {
+        int armiesToGive = (int) (fixedGamePlayModel.getGameMap().getTerritoriesCount() * INITIAL_ARMY_RATIO / fixedGamePlayModel.getPlayers().size());
+        
+        System.out.println("Testing initial armies given to the players:");
+        System.out.println("Total of " + armiesToGive + " initial armies given to each player.");
+        for (Player player : fixedGamePlayModel.getPlayers()) {
+            System.out.println("\tPlayer " + player.getPlayerID() + "'s initial unallocated armies = " +
+                    (player.getUnallocatedArmies() + player.getTerritories().size()));
+        }
+        System.out.println();
     
+        assertEquals(17, fixedGamePlayModel.getPlayers().get(0).getUnallocatedArmies() +
+                fixedGamePlayModel.getPlayers().get(0).getTerritories().size());
+        assertEquals(17, fixedGamePlayModel.getPlayers().get(1).getUnallocatedArmies() +
+                fixedGamePlayModel.getPlayers().get(1).getTerritories().size());
     }
     
     /**
@@ -116,51 +130,22 @@ public class GamePlayModelTest {
      */
     @Test
     public void assignOneArmyPerTerritoryTestCase() {
+        int armiesToGive = (int) (fixedGamePlayModel.getGameMap().getTerritoriesCount() * INITIAL_ARMY_RATIO / fixedGamePlayModel.getPlayers().size());
     
-    }
-
-    /**
-     * Game play game_entities test case.
-     */
-    @Test
-    public void gamePlayModelTestCase() {
-        // testing initialization of players' territories and armies
-        System.out.println("Testing initialization of players, their territories, " +
-                "and the number of initial armies given:");
+        System.out.println("Testing assignment of one army per territory:");
         for (Player player : fixedGamePlayModel.getPlayers()) {
-            System.out.println("player " + player.getPlayerID() + " owns "
-                    + player.getTerritories().size() + " territories, and are given "
-                    + (player.getUnallocatedArmies() + player.getTerritories().size()) + " initial armies:");
-            for (Map.Entry<String, Territory> entry : fixedGamePlayModel.getGameMap().getTerritoriesOfPlayer(player).entrySet()) {
-                System.out.println("\t" + entry.getKey());
+            int counter = 1;
+            System.out.println("\tPlayer " + player.getPlayerID() + " has " + player.getUnallocatedArmies() + " unallocated armies remaining.");
+            for (int i = 0; i < player.getTerritories().size(); i++) {
+                System.out.println("\t\t" + counter + ") " + player.getTerritories().get(i).getName() +
+                        " has " + player.getTerritories().get(i).getArmies() + " army.");
+                counter++;
             }
-            assertEquals(7, player.getTerritories().size());
-            assertEquals(19, player.getUnallocatedArmies() + player.getTerritories().size());
         }
-        assertEquals(6, fixedGamePlayModel.getPlayers().size());
         System.out.println();
-
-        // testing initialization of the deck
-        System.out.println("Deck size at the beginning of the game: " + fixedGamePlayModel.getDeck().size() + 5*6);
-        assertEquals(42, fixedGamePlayModel.getDeck().size() + 5*6);
-        System.out.println();
-
-        // testing calculation of reinforcement armies
-        System.out.println("Players' reinforcement armies:");
-        int continentValue = 0;
-        for (Player player : fixedGamePlayModel.getPlayers()) {
-            fixedGamePlayModel.setCurrentPlayer(player);
-            int initialUnallocatedArmies = fixedGamePlayModel.getCurrentPlayer().getUnallocatedArmies();
-            fixedGamePlayModel.addReinforcementForCurrPlayer();
-            System.out.println("Reinforcement armies gained by Player " + player.getPlayerID() + ": "
-                    + (fixedGamePlayModel.getCurrentPlayer().getUnallocatedArmies() - initialUnallocatedArmies));
-            for (Map.Entry<String, Continent> entry : fixedGamePlayModel.getGameMap().getContinents().entrySet()) {
-                if (fixedGamePlayModel.getCurrentPlayer().getPlayerName().compareTo(entry.getValue().getContinentOwner(fixedGamePlayModel.getGameMap())) == 0) {
-                    continentValue = entry.getValue().getControlValue();
-                }
-            }
-            assertEquals(3 + continentValue, fixedGamePlayModel.getCurrentPlayer()
-                    .getUnallocatedArmies() - initialUnallocatedArmies);
+    
+        for (Map.Entry<String, Territory> entry : fixedGamePlayModel.getGameMap().getTerritories().entrySet()) {
+            assertEquals(1, entry.getValue().getArmies());
         }
     }
 }
