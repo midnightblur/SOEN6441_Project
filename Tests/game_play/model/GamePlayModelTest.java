0102139417c0062fda1e3d29b20536e5b9daccb1
Juyoun Kim <juyoun.brian.kim@gmail.com>
2017-11-08 12:14:40 -0500

    more tests

--- Tests/game_play/model/GamePlayModelTest.java
+++ Tests/game_play/model/GamePlayModelTest.java
@@ -8,6 +8,7 @@ package game_play.model;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
+import shared_resources.game_entities.Card;
 import shared_resources.utilities.FixedGamePlayModel;
 import org.junit.Test;
 import shared_resources.game_entities.Continent;
@@ -17,6 +18,7 @@ import shared_resources.helper.GameMapHelper;
 
 
 import java.util.Map;
+import java.util.Vector;
 
 import static org.junit.Assert.assertEquals;
 
@@ -42,13 +44,12 @@ public class GamePlayModelTest {
      */
     @Test
     public void initPlayerTestCase() {
-        System.out.println("Testing initialization of players, their territories, " +
-                "and the number of initial armies given:");
-    
+        System.out.println("Testing initialization of the players:");
         System.out.println("Total of " + fixedGamePlayModel.getPlayers().size() + " players initialized.");
-        for (Player player: fixedGamePlayModel.getPlayers()) {
-            System.out.println("Player " + player.getPlayerID());
+        for (Player player : fixedGamePlayModel.getPlayers()) {
+            System.out.println("\tPlayer " + player.getPlayerID());
         }
+        System.out.println();
         
         assertEquals(2, fixedGamePlayModel.getPlayers().size());
     }
@@ -58,7 +59,14 @@ public class GamePlayModelTest {
      */
     @Test
     public void initDeckTestCase() {
-        System.out.println(fixedGamePlayModel.getGameMap().getTerritoriesCount());
+        System.out.println("Testing initialization of the deck:");
+        System.out.println("Total of " + fixedGamePlayModel.getDeck().size() + " cards initialized in the deck.");
+        int counter = 1;
+        for (Card card : fixedGamePlayModel.getDeck()) {
+            System.out.println("\t" + counter + ") Card " + card.getCardType());
+            counter++;
+        }
+        System.out.println();
         
         assertEquals(15, fixedGamePlayModel.getDeck().size());
     }
@@ -68,7 +76,31 @@ public class GamePlayModelTest {
      */
     @Test
     public void fixedDistributeTerritoriesTestCase() {
+        String p1territories = "";
+        String p2territories = "";
+       
+        System.out.println("Testing fixed distribution of territories:");
+        System.out.println("Total of " + fixedGamePlayModel.getGameMap().getTerritoriesCount() + " territories distributed.");
+        for (Player player : fixedGamePlayModel.getPlayers()) {
+            int counter = 1;
+            System.out.println("\tPlayer " + player.getPlayerID() + " owns " + player.getTerritories().size() + " territories");
+            for (int i = 0; i < player.getTerritories().size(); i++) {
+                if (player.getPlayerID() == 1) {
+                    p1territories += player.getTerritories().get(i).getName();
+                } else {
+                    p2territories += player.getTerritories().get(i).getName();
+                }
+                System.out.println("\t\t" + counter + ") " + player.getTerritories().get(i).getName());
+                counter++;
+            }
+        }
+        System.out.println();
     
+        assertEquals(13, fixedGamePlayModel.getGameMap().getTerritoriesCount());
+        assertEquals(7, fixedGamePlayModel.getPlayers().get(0).getTerritories().size());
+        assertEquals("1c2c3c4c5c6c7c", p1territories);
+        assertEquals(6, fixedGamePlayModel.getPlayers().get(1).getTerritories().size());
+        assertEquals("1t2t3t4t5t6t", p2territories);
     }
     
     /**
