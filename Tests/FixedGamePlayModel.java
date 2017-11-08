d82a9f0bf972b23b8a0b67b512ed69ce4a099015
Juyoun Kim <juyoun.brian.kim@gmail.com>
2017-11-08 01:34:25 -0500

    fixed model for gameplaymodel

--- Tests/FixedGamePlayModel.java
+++ Tests/FixedGamePlayModel.java
@@ -1,3 +1,27 @@
+import game_play.model.GamePlayModel;
+import shared_resources.helper.GameMapHelper;
+
+import java.util.Map;
+
 public class FixedGamePlayModel {
+    public static GamePlayModel getFixedGamePlayModel() {
+        /** The game play game_entities. */
+        final GamePlayModel fixedGamePlayModel = new GamePlayModel();
+    
+        /** The num of players. */
+        final int numOfPlayers = 2;
+    
+        /** The map file path. */
+        final String mapFilePath = "World.map";
+        
+        try {
+            fixedGamePlayModel.setGameMap(GameMapHelper.loadGameMap(mapFilePath));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+        
+        return fixedGamePlayModel;
+    }
 }
 
