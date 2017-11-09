2e199ae92c0af9995142a69417fb56cdda08defe
Juyoun Kim <juyoun.brian.kim@gmail.com>
2017-11-08 21:25:24 -0500

    another pull

--- Tests/shared_resources/game_entities/PlayerTest.java
+++ Tests/shared_resources/game_entities/PlayerTest.java
@@ -97,16 +97,19 @@ public class PlayerTest {
     }
     
     /**
-     *
+     * Tests the correct number of armies that can be moved during Fortification Phase.
      */
     @Test
     public void fortificationTestCase() {
         GamePlayModel tempGamePlayModel = FixedGamePlayModel.getFixedGamePlayModel();
         Player player = tempGamePlayModel.getPlayers().get(0);
         Territory sourceT = tempGamePlayModel.getGameMap().getATerritory("1c");
-        Territory targetT = tempGamePlayModel.getGameMap().getATerritory("1t");
+        Territory targetT = tempGamePlayModel.getGameMap().getATerritory("2c");
         sourceT.setArmies(0);
         targetT.setArmies(0);
+    
+        System.out.println("Testing valid number of armies that can be moved during Fortification Phase:");
+        
         
         int noOfArmies = 1;
         player.fortification(tempGamePlayModel, sourceT.getName(), targetT.getName(), noOfArmies);
