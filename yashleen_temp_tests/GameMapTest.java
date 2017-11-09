37e33e58efcae12047fc443d1a60ad2d66c19ae9
yashleenvirk <yashleenvirk@gmail.com>
2017-11-08 22:21:40 -0500

    deleting trial

---
+++ yashleen_temp_tests/GameMapTest.java
@@ -0,0 +1,52 @@
+package shared_resources.game_entities;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameMapTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void addTerritory() throws Exception {
+    }
+
+    @Test
+    public void getATerritory() throws Exception {
+    }
+
+    @Test
+    public void updateTerritory() throws Exception {
+    }
+
+    @Test
+    public void getAContinent() throws Exception {
+    }
+
+    @Test
+    public void removeTerritory() throws Exception {
+    }
+
+    @Test
+    public void addContinent() throws Exception {
+    }
+
+    @Test
+    public void updateContinent() throws Exception {
+    }
+
+    @Test
+    public void removeContinent() throws Exception {
+    }
+
+    @Test
+    public void getArbitraryTerritory() throws Exception {
+    }
+
+}
