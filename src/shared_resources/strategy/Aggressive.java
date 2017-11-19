8830af64a52540d238e0b5d96e0f4d0dc4aab193
Juyoun Kim <juyoun.brian.kim@gmail.com>
2017-11-19 13:42:26 -0500

    make trade cards function a default method

--- src/shared_resources/strategy/Aggressive.java
+++ src/shared_resources/strategy/Aggressive.java
@@ -17,78 +17,11 @@ public class Aggressive implements Strategy {
     
     @Override
     public String reinforcement(GamePlayModel gamePlayModel, Vector<String> selectedCards, Map<Territory, Integer> armiesToPlace) {
+        tradeCardsForBot(gamePlayModel, selectedCards);
+        
         Player player = gamePlayModel.getCurrentPlayer();
 
-        /* check for valid card sets to trade if the AI has 3 or more cards */
-        Vector<Card> infantryCards = new Vector<>();
-        Vector<Card> cavalryCards = new Vector<>();
-        Vector<Card> artilleryCards = new Vector<>();
-        if (player.getPlayersHand().size() >= Config.MIN_CARDS_TO_TRADE) {
-            for (int i = 0; i < player.getPlayersHand().size(); i++) {
-                Card card = player.getPlayersHand().get(i);
-                switch (card.getCardType().name()) {
-                    case "INFANTRY":
-                        infantryCards.addElement(card);
-                        break;
-                    case "CAVALRY":
-                        cavalryCards.addElement(card);
-                        break;
-                    case "ARTILLERY":
-                        artilleryCards.addElement(card);
-                        break;
-                }
-            }
-        }
         
-        /* trade cards as long as the Aggressive AI can */
-        while (true) {
-            selectedCards.clear();
-            
-            /* if there is a set of 3 cards with each different type */
-            if (infantryCards.size() != 0 && cavalryCards.size() != 0 && artilleryCards.size() != 0) {
-                selectedCards.addElement(infantryCards.firstElement().getCardType().name());
-                infantryCards.remove(0);
-                infantryCards.trimToSize();
-            
-                selectedCards.addElement(cavalryCards.firstElement().getCardType().name());
-                cavalryCards.remove(0);
-                cavalryCards.trimToSize();
-            
-                selectedCards.addElement(artilleryCards.firstElement().getCardType().name());
-                artilleryCards.remove(0);
-                artilleryCards.trimToSize();
-            }
-            /* if there are 3 infantry cards */
-            else if (infantryCards.size() >= 3) {
-                for (int i = 0; i < 3; i++) {
-                    selectedCards.addElement(infantryCards.firstElement().getCardType().name());
-                    infantryCards.remove(0);
-                    infantryCards.trimToSize();
-                }
-            }
-            /* if there are 3 cavalry cards */
-            else if (cavalryCards.size() >= 3) {
-                for (int i = 0; i < 3; i++) {
-                    selectedCards.addElement(cavalryCards.firstElement().getCardType().name());
-                    cavalryCards.remove(0);
-                    cavalryCards.trimToSize();
-                }
-            }
-            /* if there are 3 artillery cards */
-            else if (artilleryCards.size() >= 3) {
-                for (int i = 0; i < 3; i++) {
-                    selectedCards.addElement(artilleryCards.firstElement().getCardType().name());
-                    artilleryCards.remove(0);
-                    artilleryCards.trimToSize();
-                }
-            }
-            /* if cannot trade anymore */
-            else {
-                break;
-            }
-        
-            player.tradeInCards(gamePlayModel, selectedCards);
-        }
     
     
         return null;
