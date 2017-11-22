5e267335937caa3786aaaff1e3e4d997447a7e5f
Juyoun Kim <juyoun.brian.kim@gmail.com>
2017-11-22 18:22:45 -0500

    attack for aggressive

--- src/shared_resources/strategy/AggressiveBot.java
+++ src/shared_resources/strategy/AggressiveBot.java
@@ -43,59 +43,36 @@ public class AggressiveBot extends Bot {
     public void attack(GamePlayModel gamePlayModel) {
         Player player = gamePlayModel.getCurrentPlayer();
         Territory strongestTerritory = findStrongestTerritory(player.getTerritories());
-        if (hasAttackableNeighbors(strongestTerritory, gamePlayModel)) {
-        
-        } else {
-        
-        }
-    
-        // The player attacks until he decides to stop or simply cannot attack anymore
         while (true) {
-            // Make the choice whether to attack or not randomly
-            Random rand = new Random();
-            boolean doAttack = rand.nextBoolean();
-        
-            if (doAttack) {
-                Vector<Territory> territories = new Vector<>(player.getTerritories());
-                while (territories.size() > 0) {
-                    // Find a random territory
-                    int randIndex;
-                    if (territories.size() > 1) {
-                        randIndex = rand.nextInt(territories.size() - 1);
-                    } else {
-                        randIndex = 0;
-                    }
-                    Territory randomTerritory = territories.elementAt(randIndex);
-                
-                    if (randomTerritory.getArmies() >= 2) {
-                        // Find one of its neighbor owned by another player
-                        for (String neighborName : randomTerritory.getNeighbors()) {
-                            Territory neighbor = gamePlayModel.getGameMap().getATerritory(neighborName);
-                            if (!neighbor.isOwnedBy(player)) {
-                                // Declare an attack using a random choice of number of dice
-                                int maxAttackerDice = Math.min(3, randomTerritory.getArmies());
-                                int attackerDice = 1 + rand.nextInt(maxAttackerDice);
-                                int maxDefenderDice = Math.min(2, neighbor.getArmies());
-                                int defenderDice = 1 + rand.nextInt(maxDefenderDice);
-                                gamePlayModel.setCurrentBattle(new Battle(player, randomTerritory, attackerDice,
-                                        neighbor.getOwner(), neighbor, defenderDice));
-                                attackForBots(gamePlayModel);
-                                break;
+            if (hasAttackableNeighbors(strongestTerritory, gamePlayModel) && strongestTerritory.getArmies() >= 2) {
+                // Find one of its neighbor owned by another player
+                for (String neighborName : strongestTerritory.getNeighbors()) {
+                    Territory neighbor = gamePlayModel.getGameMap().getATerritory(neighborName);
+                    if (!neighbor.isOwnedBy(player)) {
+                        // Continue attacking the neighboring country until it is conquered or until it no longer can
+                        while (strongestTerritory.getArmies() >= 2 && neighbor.getArmies() != 0) {// Declare an attack using as many dice as possible
+                            int attackerDice;
+                            if (strongestTerritory.getArmies() > 3) {
+                                attackerDice = 3;
+                            } else if (strongestTerritory.getArmies() > 2) {
+                                attackerDice = 2;
+                            } else {
+                                attackerDice = 1;
                             }
+        
+                            //TODO: separate humans players' decision to determine the number of def dice
+                            Random rand = new Random();
+                            int maxDefenderDice = Math.min(2, neighbor.getArmies());
+                            int defenderDice = 1 + rand.nextInt(maxDefenderDice);
+                            gamePlayModel.setCurrentBattle(new Battle(player, strongestTerritory, attackerDice,
+                                    neighbor.getOwner(), neighbor, defenderDice));
+                            attackForBots(gamePlayModel);
                         }
-                        break;
                     }
-                
-                    // If this territory choice cannot attack any territory, prevent it from being chosen again
-                    territories.remove(randomTerritory);
-                }
-            
-                if (territories.size() == 0) {
-                    log.append("        " + player.getPlayerName() + " cannot attack anymore");
-                    return;
+                    break;
                 }
             } else {
-                log.append("        " + player.getPlayerName() + " quits attacking phase");
+                log.append("        " + player.getPlayerName() + " quits attacking phase (strongest territory cannot attack)");
                 return;
             }
         }
