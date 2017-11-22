71a6c38692984ac531a56f5fe65b0c424db41b0e
Juyoun Kim <juyoun.brian.kim@gmail.com>
2017-11-22 12:36:43 -0500

    helper fxns in aggressive

--- src/shared_resources/strategy/AggressiveBot.java
+++ src/shared_resources/strategy/AggressiveBot.java
@@ -6,7 +6,10 @@ import shared_resources.game_entities.Territory;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 import java.util.Vector;
+
+import static shared_resources.utilities.Config.log;
 
 /**
  * The aggressive AI strategy. Reinforces its strongest country, then always attacks with it until it
@@ -20,16 +23,9 @@ public class AggressiveBot extends Bot {
 
         /* find the strongest territory and reinforce that territory */
         Player player = gamePlayModel.getCurrentPlayer();
-        Territory strongestTerritory = null;
-        for (Territory territory : player.getTerritories()) {
-            if (strongestTerritory == null) {
-                strongestTerritory = territory;
-            } else {
-                if (territory.getArmies() > strongestTerritory.getArmies()) {
-                    strongestTerritory = territory;
-                }
-            }
-        }
+        Territory strongestTerritory = findStrongestTerritory(player.getTerritories());
+        log.append("    " + player.getPlayerName() + "'s strongest territory is " +
+                strongestTerritory.getName() +" having " + strongestTerritory.getArmies() + " armies");
         if (armiesToPlace != null) {
             armiesToPlace.clear();
         } else {
@@ -44,11 +40,100 @@ public class AggressiveBot extends Bot {
 
     @Override
     public void attack(GamePlayModel gamePlayModel) {
-
+    
     }
 
     @Override
     public String fortification(GamePlayModel gamePlayModel, String sourceTerritory, String targetTerritory, int noOfArmies) {
+        /* if the strongest territory has attackable neighbors, fortify the strongest territory */
+        Player player = gamePlayModel.getCurrentPlayer();
+        Territory strongestTerritory = findStrongestTerritory(player.getTerritories());
+        if (hasAttackableNeighbors(strongestTerritory, gamePlayModel)) {
+        
+        }
+        /* if the strongest territory does not have any attackable neighbors, fortify an adjacent territory owned by the player (priority to the adjacent territory that has an attackable neighbor) */
+        else {
+        
+        }
         return null;
+    }
+    
+    /**
+     * Find a random neighbor territory (assumes that all neighbors are owned by the player)
+     *
+     * @param gamePlayModel the game play model
+     * @param neighbors     the neighbors list
+     *
+     * @return random neighbor territory
+     */
+    private Territory findRandomNeighbor(GamePlayModel gamePlayModel, Vector<String> neighbors) {
+        Random rand = new Random();
+        int randIndex = rand.nextInt(neighbors.size());
+        return gamePlayModel.getGameMap().getATerritory(neighbors.get(randIndex));
+    }
+    
+    /**
+     * Find the territory owned by a player having the least number of armies
+     *
+     * @param gamePlayModel the game play model
+     * @param neighbors     the neighbors list
+     *
+     * @return the weakest territory
+     */
+    private Territory findStrongestNeighbor(GamePlayModel gamePlayModel, Vector<String> neighbors) {
+        Territory strongestNeighbor = null;
+        for (String neighborName : neighbors) {
+            Territory neighbor = gamePlayModel.getGameMap().getATerritory(neighborName);
+            if (neighbor.getOwner() == gamePlayModel.getCurrentPlayer()) {
+                if (strongestNeighbor == null) {
+                    strongestNeighbor = neighbor;
+                } else {
+                    if (neighbor.getArmies() > strongestNeighbor.getArmies()) {
+                        strongestNeighbor = neighbor;
+                    }
+                }
+            }
+        }
+        return strongestNeighbor;
+    }
+    
+    /**
+     * Checks to see if a territory has any attackable neighbors (neighboring territories
+     * that are not owned by the same player).
+     *
+     * @param territory the territory to check for any attackable neighbors
+     * @param gamePlayModel the game play model
+     *
+     * @return boolean true if there are any attackable neighbors, false if there aren't
+     */
+    private boolean hasAttackableNeighbors(Territory territory, GamePlayModel gamePlayModel) {
+        for (int i = 0; i < territory.getNeighbors().size(); i++) {
+            if (gamePlayModel.getGameMap().getATerritory(territory.getNeighbors().get(i))
+                    .getOwner() != gamePlayModel.getCurrentPlayer()) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    /**
+     * Find the territory owned by a player having the most number of armies
+     *
+     * @param territories the territories list
+     *
+     * @return the weakest territory
+     */
+    private Territory findStrongestTerritory(Vector<Territory> territories) {
+        Territory strongestTerritory = null;
+        for (Territory territory : territories) {
+            if (strongestTerritory == null) {
+                strongestTerritory = territory;
+            } else {
+                if (territory.getArmies() > strongestTerritory.getArmies()) {
+                    strongestTerritory = territory;
+                }
+            }
+        }
+        return strongestTerritory;
     }
 }
