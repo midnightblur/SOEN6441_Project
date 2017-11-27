545942dd6f5970592c310f98e4d67e92381c95dd
Juyoun Kim <juyoun.brian.kim@gmail.com>
2017-11-27 12:05:09 -0500

    remove todo in botChooseDefendingDice() method

--- src/shared_resources/game_entities/Player.java
+++ src/shared_resources/game_entities/Player.java
@@ -616,7 +616,6 @@ public class Player implements Serializable {
      * @return the number of dice to defend
      */
     public int botChooseDefendingDice(int maxDefendingDice) {
-        // TODO: implement Strategy pattern for this function
         int defendingDice = 1;
         if (this.isRandomBot()) {
             // Choose number of dice randomly
