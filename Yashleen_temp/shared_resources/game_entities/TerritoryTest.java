0019d0870cfb4c49412442ef92b1d7a92051808d
yashleenvirk <yashleenvirk@gmail.com>
2017-11-27 23:13:13 -0500

    PlayerTest-commenting

--- Yashleen_temp/shared_resources/game_entities/TerritoryTest.java
+++ Yashleen_temp/shared_resources/game_entities/TerritoryTest.java
@@ -1,31 +1,31 @@
-/*
- * Risk Game Team 2
- * TerritoryTest.java
- * Version 1.0
- * Oct 18, 2017
- */
-package shared_resources.game_entities;
-
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-
-/**
- * This test case is used to test getName function of the territory.
- */
-public class TerritoryTest {
-
-    /** The territory. */
-    Territory territory = new Territory("Canada");
-
-    /**
-     * Gets the name.
-     *
-     * @throws Exception the exception
-     */
-    @Test
-    public void getName() throws Exception {
-        assertEquals("Canada", territory.getName());
-    }
-
-}
+///*
+// * Risk Game Team 2
+// * TerritoryTest.java
+// * Version 1.0
+// * Oct 18, 2017
+// */
+//package shared_resources.game_entities;
+//
+//import org.junit.Test;
+//
+//import static org.junit.Assert.assertEquals;
+//
+///**
+// * This test case is used to test getName function of the territory.
+// */
+//public class TerritoryTest {
+//
+//    /** The territory. */
+//    Territory territory = new Territory("Canada");
+//
+//    /**
+//     * Gets the name.
+//     *
+//     * @throws Exception the exception
+//     */
+//    @Test
+//    public void getName() throws Exception {
+//        assertEquals("Canada", territory.getName());
+//    }
+//
+//}
