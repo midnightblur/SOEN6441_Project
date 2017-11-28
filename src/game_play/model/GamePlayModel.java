04d83a65dc297a9836fcfce3e2a3394c626da8a5
Quoc Minh VU <minhvq27@gmail.com>
2017-11-27 19:53:19 -0500

    Small fix

--- src/game_play/model/GamePlayModel.java
+++ src/game_play/model/GamePlayModel.java
@@ -1087,10 +1087,6 @@ public class GamePlayModel extends Observable implements Serializable {
             botsReinforcement();
             
             botsAttack();
-        } else {
-            // TODO: pop-up continue dialog and reset turnCounter
-            // view will observe this change and click an already existing invisible button
-            // this button is listened by controller that pops-up the frame
         }
     }
     
