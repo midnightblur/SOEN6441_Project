a99b55f6ab476ac801715fe452341230f79c47f1
yashleenvirk <yashleenvirk@gmail.com>
2017-11-29 14:23:45 -0500

    Tests

---
+++ Yashleen_temp/AllTestSuite.java
@@ -0,0 +1,34 @@
+/*
+ * Risk Game Team 2
+ * AllTestSuite.java
+ * Version 1.0
+ * Oct 18, 2017
+ */
+
+//import game_play.model.GamePlayModelTest;
+
+import game_play.model.GamePlayModelTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+import shared_resources.game_entities.ContinentTest;
+import shared_resources.game_entities.DiceTest;
+import shared_resources.game_entities.PlayerTest;
+import shared_resources.game_entities.TerritoryTest;
+import shared_resources.helper.GameMapHelperTest;
+
+/**
+ * The Class AllTestSuite used to run unit test classes
+ */
+@RunWith(Suite.class)
+@SuiteClasses({
+        GameMapHelperTest.class,
+        ContinentTest.class,
+        DiceTest.class,
+        PlayerTest.class,
+        GamePlayModelTest.class,
+        TerritoryTest.class,
+})
+
+public class AllTestSuite {
+}
